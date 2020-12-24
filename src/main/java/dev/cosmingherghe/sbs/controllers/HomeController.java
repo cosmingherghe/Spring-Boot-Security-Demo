59135f45a1740725a5dc67cb2f390e224cc4c8ef
Cosmin Gherghe <cosminlinux@gmail.com>
2020-12-24 00:41:50 +0000

    Adding layouts fragments for menu

---
+++ src/main/java/dev/cosmingherghe/sbs/controllers/HomeController.java
@@ -0,0 +1,12 @@
+package dev.cosmingherghe.sbs.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+    public String index() {
+        return "index";
+    }
+}
