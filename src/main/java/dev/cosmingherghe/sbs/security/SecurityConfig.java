5601546207d6a3989a3ee283cbd4792af338c9ec
Cosmin Gherghe <cosminlinux@gmail.com>
2020-12-24 23:34:08 +0000

    adding WebSecurityConfigurerAdapter Method that defines data base for users

---
+++ src/main/java/dev/cosmingherghe/sbs/security/SecurityConfig.java
@@ -0,0 +1,23 @@
+package dev.cosmingherghe.sbs.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    //Method that defines data base for users
+    @Override
+    protected void configure(AuthenticationManagerBuilder managerBuilder) throws Exception {
+        managerBuilder
+                .inMemoryAuthentication()
+                .withUser("admin").password("admin").roles("ADMIN")
+                .and()
+                .withUser("user").password("user").roles("USER");
+    }
+
+}
